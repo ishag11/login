2c47104c1b1b9202bea0f355e60c77ad051cdfa9
meenamurugesan-lti <Meena.Murugesan@lntinfotech.com>
2022-03-09 12:04:25 +0530

    first commit

---
+++ src/main/java/com/example/springjwt/dto/LoginStatus.java
@@ -0,0 +1,30 @@
+package com.example.springjwt.dto;
+
+import org.springframework.http.HttpStatus;
+
+public class LoginStatus extends Status {
+	
+	private HttpStatus httpstatus;
+	private String username;
+	private String email;
+	public HttpStatus getHttpstatus() {
+		return httpstatus;
+	}
+	public void setHttpstatus(HttpStatus httpstatus) {
+		this.httpstatus = httpstatus;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
+
